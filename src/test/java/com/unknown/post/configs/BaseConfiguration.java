20837d65701cea6172fd27f3fe40dab30934ef2f
ender019 <ender-fox@mail.ru>
2025-06-28 15:41:00 +0300

    Обновление конфигурации для тестов.

--- src/test/java/com/unknown/post/configs/BaseConfiguration.java
+++ src/test/java/com/unknown/post/configs/BaseConfiguration.java
@@ -1,11 +1,14 @@
 package com.unknown.post.configs;
 
+import io.mongock.runner.springboot.EnableMongock;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.MongoDBContainer;
 
+
 @TestConfiguration
+@EnableMongock
 public class BaseConfiguration {
     private static final MongoDBContainer container;
 
