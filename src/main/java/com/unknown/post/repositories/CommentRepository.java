5ccb40d9cf8836ba1feb1608376a9adf69eb2389
ender019 <ender-fox@mail.ru>
2025-07-25 15:35:56 +0300

    Обновление репозиториев

--- src/main/java/com/unknown/post/repositories/CommentRepository.java
+++ src/main/java/com/unknown/post/repositories/CommentRepository.java
@@ -2,6 +2,8 @@ package com.unknown.post.repositories;
 
 import com.unknown.post.entities.Comment;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.Update;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +16,11 @@ public interface CommentRepository extends MongoRepository<Comment, String> {
     List<Comment> findCommentsByAuthor(String authorId);
 
     void deleteCommentById(String id);
+
+    void deleteCommentByAuthor(String userId);
+
+    @Query("{ 'author' : ?0 }")
+    @Update("{ $set: { 'author': ?1 } }")
+    void updateAuthorByAuthor(String userId, String updatedAuthor);
+
 }
